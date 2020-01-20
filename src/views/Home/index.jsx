79df43537302a98540dbfe6133d2d0d7be62bd12
ifmiss <185098535@qq.com>
2020-01-20 21:52:51 +0800

    update: home index.js

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -2,10 +2,32 @@ import React, {
   useEffect
 } from 'react'
 
+{{#equal useStore 'redux'}}
+import {
+  connect
+} from 'react-redux'
+
+import HomeAction from '@store/actions'
+{{/equal}}
+
 const Home = () => {
   return (
-    <div>this is home</div>
+    <>
+      <div>this is home</div>
+      <Hello/>
+      <span>{props.count}</span>
+      <div onClick={props.addCount}> + 1</div>
+      <div onClick={props.reduceCount}> - 1</div>
+    </>
   )
 }
 
+{{#equal useStore 'redux'}}
+export default connect(
+  ({home}) => home,
+  HomeAction
+)(Home)
+{{/equal}}
+{{#equal useStore 'none'}}
 export default Home
+{{/equal}}
