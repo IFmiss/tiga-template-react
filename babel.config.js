149525edfb0d51f7866e93c991740dcaafbc4d31
ifmiss <185098535@qq.com>
2020-01-21 22:51:53 +0800

    update: 环境配置

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,21 @@
 module.exports = {
   "presets": [
-    ["@babel/preset-env", {
-      "modules": "false"
-    }],
+    [
+      "@babel/preset-env", {
+        modules: false
+      }
+    ],
     ["@babel/preset-react"]
   ],
   plugins: [
     [
       "@babel/plugin-proposal-decorators", {
-        "legacy": true 
+        legacy: true 
+      }
+    ],
+    [
+      "@babel/plugin-proposal-class-properties", {
+        loose : true
       }
     ],
     ["@babel/plugin-syntax-dynamic-import"],
