1baf7ee7096bd449652a322cb5a6e793b041374d
ifmiss <185098535@qq.com>
2020-01-21 13:30:15 +0800

    update: store ts

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import '@styles/reset.css'
 
+{{#equal useStore 'redux'}}
+import { Provider } from 'react-redux';
+import store from '@store/index';
+{{/equal}}
+{{#if useRouter}}
 import App from './app'
+{{/if}}
+{{#unless useRouter}}
+import App from '@views/home'
+{{/unless}}
 
 ReactDOM.render(
-  <App/>,
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 )
