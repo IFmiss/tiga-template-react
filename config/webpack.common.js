cf512277dccd04349cc313372c5c84d4399ef481
ifmiss <185098535@qq.com>
2020-05-11 16:02:24 +0800

    update: 更新webpack配置

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -30,29 +30,17 @@ module.exports = {
   module: {
 		rules: [
 			{
+				{{#if useTypeScript}}
+				test: /\.(js|jsx|ts|tsx)$/,
+				{{/if}}
+				{{#unless useTypeScript}}
 				test: /\.(js|jsx)$/,
+				{{/unless}}
 				loader: 'babel-loader',
 				query: {
 					compact: false
 				}
       },
-      {{#if useTypeScript}}
-			{
-				test: /\.(ts|tsx)$/,
-				use: [
-					{loader: 'babel-loader',},
-					{
-						loader: 'ts-loader',
-						options: {
-							// 加快编译速度
-							transpileOnly: true,
-							// 指定特定的ts编译配置，为了区分脚本的ts配置
-							configFile: path.resolve(__dirname, './../tsconfig.json')
-						}
-					}
-				]
-			},
-			{{/if}}
       {
         test: /\.(c)ss$/,
         use: [
